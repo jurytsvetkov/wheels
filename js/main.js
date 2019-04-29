6e6f7a8b03c0a16e16b857d2d57e3135b04eb3e4
Jury Tsvetkov <crmsnkng@ya.ru>
2019-04-29 03:13:53 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    
+    // page2scroll
+    $("nav a,footer a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+	    highlightSelector:"nav a"
+	});
+    
+    $(document).ready(function(){
+        $("#navigator").sticky({topSpacing:0});
+    });
+    
+}); 
